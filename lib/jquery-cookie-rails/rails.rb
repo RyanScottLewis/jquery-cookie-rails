d25d9ebba389953ecdd80de89904cb2a824bfda4
Piotr Kuczynski <piotr.kuczynski@here.com>
2014-05-23 12:35:54 +0200

    Remove unused version.rb

--- lib/jquery-cookie-rails/rails.rb
+++ lib/jquery-cookie-rails/rails.rb
@@ -1,2 +1,1 @@
 require 'jquery-cookie-rails/rails/engine'
-require 'jquery-cookie-rails/rails/version'
--- lib/jquery-cookie-rails/rails/version.rb
+++
@@ -1,6 +0,0 @@
-module JqueryCookie
-  module Rails
-    VERSION    = "1.2.0"
-    JS_VERSION = "1.2.0"
-  end
-end
